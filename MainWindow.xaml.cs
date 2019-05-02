6fea38e4e76044985390982465baa1d14bca9ba0
JoshDegazio <36270908+JoshDegazio@users.noreply.github.com>
2019-05-02 10:52:39 -0400

    Project Initialization

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿/* ToDo (Anyone): Add stuff here
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace snakeGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //ToDo (Cam): Globals
+
+        //ToDo (Josh): GameTimer, Enum
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           
+        }
+        //ToDo (Josh): Tick Method
+
+        //ToDo (Anyone): Other Methods
+
+        //ToDo (Josh): Leaderboard/Scores Methods
+
+        //ToDo (Cam): Quit Game Method
+
+        //ToDo (Cam): Game Over Method
+
+        //ToDo (Dave): Start Game Method
+
+        //ToDo (Dave): Create Grid Method
+
+
+    }
+}
