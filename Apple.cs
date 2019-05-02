b924c26458e669e342febc4f831cf152b3e162c0
davidlaughton11 <47399940+davidlaughton11@users.noreply.github.com>
2019-05-02 11:24:14 -0400

    Add files via upload

--- Apple.cs
+++ Apple.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace snakeGame
 {
     class Apple
     {
+        //David
+        private static int randomNumberMethod()
+        {
+            //word generation for easy + hard
+            int randomnumber;
+            //generates random number within 0-20
+            Random r = new Random();
+            randomnumber = r.Next(17);
+            return randomnumber;
+        }
         //ToDo (Dave): Globals
+        readonly Point position;
 
+       
         //ToDo (Josh): Methods
     }
 }
