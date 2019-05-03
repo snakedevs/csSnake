601dbf977d5c508e756ce23ff1a05dda9b94b255
JoshDegazio <36270908+JoshDegazio@users.noreply.github.com>
2019-05-03 11:14:33 -0400

    Add files via upload

--- Apple.cs
+++ Apple.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace snakeGame
 {
@@ -19,10 +22,46 @@ namespace snakeGame
             randomnumber = r.Next(17);
             return randomnumber;
         }
-        //ToDo (Dave): Globals
-        readonly Point position;
+        private Snake s;
 
-       
-        //ToDo (Josh): Methods
+        public Point Position { get; private set; }
+        private Rectangle apple;
+        private int height = 44;
+        private int width = 44;
+
+        public Apple(MainWindow window)
+        {
+            Generate();
+            window.GameCanvas.Children.Add(apple);
+            Canvas.SetTop(apple, Position.Y);
+            Canvas.SetLeft(apple, Position.X);
+            
+            
+        }
+
+        private void Generate()
+        {
+            Position = RandomPos(s);
+            apple = new Rectangle();
+            apple.Height = 44;
+            apple.Width = 44;
+            apple.Fill = Brushes.DarkRed;
+        }
+
+        private Point RandomPos(Snake s)
+        {
+            Point tempPoint = new Point();
+
+            tempPoint.X = randomNumberMethod();
+            tempPoint.Y = randomNumberMethod();
+
+            if (s.pos.x == tempPoint.X && s.pos.y == tempPoint.Y)
+            {
+                tempPoint = RandomPos(s);
+            }
+
+            return tempPoint;
+        }
+
     }
 }
