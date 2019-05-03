6ae7d834dd100d2ceb547ee901524a3187ef114d
davidlaughton11 <47399940+davidlaughton11@users.noreply.github.com>
2019-05-03 10:24:39 -0400

    Add files via upload

--- Snake.cs
+++ Snake.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Snake
 {
     class Snake
     {
-        //Dave
-        //ToDo: Globals
+        //Dave        
+        Rect[] snake;
+        public int trail { get; private set; }
+        public Point headPos { get; private set; }
+        public int score { get; private set; }
 
         //Cam
         //ToDo: Snake
