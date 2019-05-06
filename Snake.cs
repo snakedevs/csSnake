d959bda28c12faf47ea85b46b13f6241da1bff40
312639 <47399971+312639@users.noreply.github.com>
2019-05-06 14:27:53 -0400

    Update Snake.cs

--- Snake.cs
+++ Snake.cs
@@ -1,11 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
-namespace Snake
+namespace snakeGame
 {
     class Snake
     {
@@ -14,8 +17,26 @@ namespace Snake
         public int trail { get; private set; }
         public Point headPos { get; private set; }
         public int score { get; private set; }
+        private int velocity = 3;
+
 
         //Cam
+        public Snake(Canvas canvas)
+            {
+
+            Rectangle a = new Rectangle();
+            a.Height = 100;
+            a.Width = 100;
+            canvas.Children.Add(a);
+            Canvas.SetLeft(a, 100);
+            Canvas.SetTop(a, 100);
+            a.Fill = Brushes.Blue;
+
+            }
+       
+            
+        
         //ToDo: Snake
+        
     }
 }
